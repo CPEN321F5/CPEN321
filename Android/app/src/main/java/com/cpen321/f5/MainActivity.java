20de05d46c42777196777eb9fcf925bdd1fec46f
Sam Gu <gupangmk2@gmail.com>
2022-07-04 18:36:49 -0700

    android studio project

---
+++ Android/app/src/main/java/com/cpen321/f5/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cpen321.f5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //initial commit
+    }
+}
