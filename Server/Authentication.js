7f8c42da4bcc538661b3a2f27062dc48a3f1d701
Sam Gu <gupangmk2@gmail.com>
2022-07-06 11:19:25 -0700

    userdb v0.1

--- Server/Authentication.js
+++ Server/Authentication.js
@@ -5,22 +5,25 @@ const User_Database = require('./UserDB.js')
 
 function Auth_module(){
     console.log("initializing Auth Module")
-    this.user_db = new User_Database("mongodb://localhost:27017", "user")
+    this.user_db = new User_Database("mongodb://localhost:27017", "User")
 } 
 
-//take user id for signin, if user is new, resolves to indicating the user is new
+//take user id for signin, if user is new, resolves to new user profile
 //resolve to the user profile found in UserDB otherwise.
+//the status in the user profile indicate user status, if status is new user, front end should prompt to update profile
 Auth_module.prototype.signin = function(user_id){
-    return this.user_db.userExist(user_id).then((user_exist) => 
+    console.log("[Auth] Signin Request for " + user_id)
+    return this.user_db.getProfile(user_id).then((user_profile) => 
         new Promise((resolve, reject) => {
-            if(user_exist){
+            if(user_profile){
                 //not a new user, resolving to the user profile
-                console.log("found user with user_id" + user_id)
-                resolve(this.user_db.getProfile(user_id))
+                console.log("[Auth] found user with user_id " + user_id)
+                resolve(user_profile)
             }
             else{
-                //a new user, adding to the data base
-                default_user = {
+                //user_profile is null, a new user, adding to the data base
+                console.log("[Auth] no profile for user id " + user_id)
+                var default_user = {
                     //Basic info
                     "UserID"            : user_id,
                     "Email"             : "N/A",
@@ -39,18 +42,45 @@ Auth_module.prototype.signin = function(user_id){
                     
                     "Status"            : "new_user"
                 }
-                this.user_db.addUser(default_user)
-                resolve({"status" : "new_user"})
+                this.user_db.addUser(default_user).then(result => {
+                    console.log("[Auth] Successfully add user, insert id " + result.insertedId)
+                    resolve(default_user)
+                })
             }
         })
     )
 }
 
 //returns result from userDB
-Auth_module.prototype.updateProfile = function(query){
-    return this.user_db.updateProfile(query)
+Auth_module.prototype.updateProfile = function(profile){
+    return this.user_db.updateProfile(profile)
+}
+
+Auth_module.prototype.removeUser = function(user_id){
+    return this.user_db.removeUser(user_id)
 }
 
 am = new Auth_module()
 
-am.signin(123).then(result => {console.log(result)})
+am.signin(1234).then(result => {console.log(result)})
+// am.removeUser(1234).then(result => {console.log(result)})
+profile_user = {
+    //Basic info
+    "UserID"            : 1234,
+    "Email"             : "exampleemail",
+    "DisplayName"       : "N/A",
+    "FirstName"         : "N/A",
+    "LastName"          : "N/A",
+    //Address
+    "Unit"              : "N/A",
+    "Address_line_1"    : "N/A",
+    "Address_line_2"    : "N/A",
+    "City"              : "N/A",
+    "Province"          : "N/A",
+    "Country"           : "N/A",
+    "ZIP_code"          : "N/A",
+    "Phone"             : "N/A",
+    
+    "Status"            : "normal"
+}
+//am.updateProfile(profile_user)
--- Server/UserDB.js
+++ Server/UserDB.js
@@ -25,41 +25,75 @@ function Database(mongoUrl, dbName){
 }
 
 //get the profile for a user
+//resolve to null if don't exist
 Database.prototype.getProfile = function(user_id){
     return this.connected.then(
         db => new Promise((resolve, reject) => {
-            resolve({"id" : "test_id"})
+            var query = {UserID : user_id}
+            var user_profile = db.collection("Profile").findOne(query)
+            resolve(user_profile)
         })
     )
 }
 
-//check if user exist
-Database.prototype.userExist = function(user_id){
-    return this.connected.then(
-        db => new Promise((resolve, reject) => {
-            resolve(false)
-        })
-    )
-}
-
-//add an user to the database, resolve to true if successful
+//add an user to the database, resolve to insert result when done
 Database.prototype.addUser = function(user){
     return this.connected.then(
         db => new Promise((resolve, reject) => {
             //add user to database
             console.log("[UserDB] Adding a user")
-            console.log(user)
-            resolve(true)
+            var result = db.collection("Profile").insertOne(user)
+            resolve(result)
         })
     )
 }
 
 //updates the user profile based on the query, return ture if successful
-Database.prototype.updateProfile = function(query){
+Database.prototype.updateProfile = function(profile){
     return this.connected.then(
         db => new Promise((resolve, reject) => {
-            console.log("[UserDB] updating user profile")
-            resolve(true);
+            console.log("[UserDB] updating user profile for user " + profile.UserID)
+
+            //configuring the parameter for update
+            const filter = { UserID: profile.UserID };
+            const options = { upsert: true };
+            const update_profile = {$set: profile}
+
+            var result = db.collection("Profile").updateOne(filter, update_profile, options)
+            resolve(result);
+
+        }).then(result =>{
+            console.log("[UserDB] found " + result.matchedCount + "document, updated " + result.modifiedCount + "documents")
+            if (result.modifiedCount >= 1){
+                console.log("[UserDB] successfully updated profile")
+                return true
+            }else{
+                console.log("[UserDB] failed to updated profile, inserted as new profile")
+                return false
+            }
+        })
+    )
+}
+
+//remove a user with user_id , resolve to true if successful, false if user does not exist
+Database.prototype.removeUser = function(user_id){
+    return this.connected.then(
+        db => new Promise((resolve, reject) => {
+            //removing a user
+            console.log("[UserDB] Removing a user with id " + user_id)
+            var query = {UserID : user_id}
+            var result = db.collection("Profile").deleteOne(query)
+            resolve(result)
+        }).then(result =>{
+            console.log("[UserDB] Removed " + result.deletedCount + " Users")
+            if(result.deletedCount === 1){
+                console.log("[UserDB] Delete Successful userid " + user_id)
+                return true
+            }
+            else{
+                console.log("[UserDB] Delete failed, Did not find user " + user_id)
+                return false
+            }
         })
     )
 }
