215eda7b99ab4b9bc4ffb37c97647a1c8041d3f6
daijun-wang <domi.daijunwang@gmail.com>
2022-07-06 22:09:26 -0700

    Item Description Page Added, BE Not Connected

--- Android/app/src/main/java/com/cpen321/f5/MainUI.java
+++ Android/app/src/main/java/com/cpen321/f5/MainUI.java
@@ -11,7 +11,10 @@ public class MainUI extends AppCompatActivity
 {
 
     private static final String TAG = "MainUI";
+
     private Button profileButton;
+    private Button itemButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,6 +31,18 @@ public class MainUI extends AppCompatActivity
             {
                 Intent profileIntent = new Intent(MainUI.this, Profile.class);
                 startActivity(profileIntent);
+            }
+        });
+
+        itemButton = findViewById(R.id.item_button);
+        itemButton.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                Intent itemIntent = new Intent(MainUI.this, Item.class);
+                startActivity(itemIntent);
             }
         });
     }
--- Android/app/src/main/java/com/cpen321/f5/Profile.java
+++ Android/app/src/main/java/com/cpen321/f5/Profile.java
@@ -2,18 +2,34 @@ package com.cpen321.f5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
 
-    TextView nameOfUser_caption;
+    private static final String TAG = "Profile";
+    private Button itemsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        itemsButton = findViewById(R.id.items_button);
+        itemsButton.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                Intent profileIntent = new Intent(Profile.this, ItemsList.class);
+                startActivity(profileIntent);
+            }
+        });
 
     }
 }
