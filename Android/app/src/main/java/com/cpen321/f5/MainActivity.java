954517c185dcc673755942172f5051e69532f77d
Yitong Tang <nb.yitong@gmail.com>
2022-07-07 01:14:19 -0700

    implement 1. the main page UI after login 2. Search 3. Post items 4. checkout google pay page

--- Android/app/src/main/java/com/cpen321/f5/MainActivity.java
+++ Android/app/src/main/java/com/cpen321/f5/MainActivity.java
@@ -2,14 +2,45 @@ package com.cpen321.f5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button searchButton;
+    private Button postButton;
+    private Button checkoutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //initial commit
+        searchButton = findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent searchItem = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(searchItem);
+            }
+        });
+
+        postButton = findViewById(R.id.post_button);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent postItem = new Intent(MainActivity.this, PostActivity.class);
+                startActivity(postItem);
+            }
+        });
+
+        checkoutButton = findViewById(R.id.checkout_button);
+        checkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent checkoutActivity = new Intent(MainActivity.this, CheckoutActivity.class);
+                startActivity(checkoutActivity);
+            }
+        });
     }
 }
