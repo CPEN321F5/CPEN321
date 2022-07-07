99cd7ede6e522ff102802b32b4ad07da133472a4
daijun-wang <domi.daijunwang@gmail.com>
2022-07-07 16:56:38 -0700

    Profile Button Connected to Main UI

--- Android/app/src/main/java/com/cpen321/f5/MainUI.java
+++ Android/app/src/main/java/com/cpen321/f5/MainUI.java
@@ -6,11 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainUI extends AppCompatActivity {
     private Button searchButton;
     private Button postButton;
     private Button checkoutButton;
+    private Button profileButton;
+    private ImageButton IB1;
+    private ImageButton IB2;
+    private ImageButton IB3;
+    private ImageButton IB4;
+    private ImageButton IB5;
+    private ImageButton IB6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +49,69 @@ public class MainUI extends AppCompatActivity {
             public void onClick(View v) {
                 Intent checkoutActivity = new Intent(MainUI.this, CheckoutActivity.class);
                 startActivity(checkoutActivity);
+            }
+        });
+
+        profileButton = findViewById(R.id.profile_button);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profileActivity = new Intent(MainUI.this, Profile.class);
+                startActivity(profileActivity);
+            }
+        });
+
+        IB1 = findViewById(R.id.imageButton01);
+        IB1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IB1Activity = new Intent(MainUI.this, Item.class);
+                startActivity(IB1Activity);
+            }
+        });
+
+        IB2 = findViewById(R.id.imageButton02);
+        IB2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IB2Activity = new Intent(MainUI.this, Item.class);
+                startActivity(IB2Activity);
+            }
+        });
+
+        IB3 = findViewById(R.id.imageButton03);
+        IB3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IB2Activity = new Intent(MainUI.this, Item.class);
+                startActivity(IB2Activity);
+            }
+        });
+
+        IB4 = findViewById(R.id.imageButton04);
+        IB4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IB4Activity = new Intent(MainUI.this, Item.class);
+                startActivity(IB4Activity);
+            }
+        });
+
+        IB5 = findViewById(R.id.imageButton05);
+        IB5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IB5Activity = new Intent(MainUI.this, Item.class);
+                startActivity(IB5Activity);
+            }
+        });
+
+        IB6 = findViewById(R.id.imageButton06);
+        IB6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IB6Activity = new Intent(MainUI.this, Item.class);
+                startActivity(IB6Activity);
             }
         });
     }
