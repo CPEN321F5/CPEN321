b77685732ed17a170c5b6fde7afa467723e363ed
Yitong Tang <nb.yitong@gmail.com>
2022-07-09 18:01:07 -0700

    upt PostActivity posts JSON to server end

--- Android/app/src/main/java/com/cpen321/f5/bidActivity.java
+++ Android/app/src/main/java/com/cpen321/f5/bidActivity.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Item extends AppCompatActivity {
-
-    private static final String TAG = "Item";
+public class bidActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item);
+        setContentView(R.layout.activity_bid);
     }
 }
