21800a9b8d15e7a467cc7dcd6d5f308d40c91f53
daijun-wang <domi.daijunwang@gmail.com>
2022-07-21 01:27:01 -0700

    Testing Change for Codacy

--- Android/app/src/androidTest/java/espresso/EspressoTest.java
+++ Android/app/src/androidTest/java/espresso/EspressoTest.java
@@ -10,8 +10,8 @@ import androidx.test.espresso.intent.rule.IntentsTestRule;
 
 import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
+//import androidx.test.espresso.matcher.ViewMatchers;
+//import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
