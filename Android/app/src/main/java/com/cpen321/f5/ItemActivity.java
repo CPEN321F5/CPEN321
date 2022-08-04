09b656dce7a8a6b05a81fc4c589b2ce44a1d7774
xuwt <xu_wanting2017@163.com>
2022-08-03 23:11:30 -0700

    fix recyclerview cover problem

--- Android/app/src/main/java/com/cpen321/f5/ItemActivity.java
+++ Android/app/src/main/java/com/cpen321/f5/ItemActivity.java
@@ -24,7 +24,6 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
