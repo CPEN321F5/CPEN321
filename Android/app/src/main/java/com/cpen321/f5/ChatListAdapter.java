48ac38493578711f09b2358fe79dfb5e8a715831
Sam Gu <gupangmk2@gmail.com>
2022-08-04 20:10:47 -0700

    delete conversation implemented

--- Android/app/src/main/java/com/cpen321/f5/ChatListAdapter.java
+++ Android/app/src/main/java/com/cpen321/f5/ChatListAdapter.java
@@ -1,15 +1,27 @@
 package com.cpen321.f5;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,6 +31,7 @@ public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.Single
     private LayoutInflater chatListInflate;
     private JSONArray coresChats= new JSONArray();
     String chat_get_url = "http://20.106.78.177:8081/chat/getconversation/";
+    String chat_delete_url = "http://20.106.78.177:8081/chat/removeconversation/";
 
     public ChatListAdapter(LayoutInflater chatListInflate){
 
@@ -77,8 +90,30 @@ public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.Single
 
     // need to implement with backend
     public void deleteList(int position) {
+        RequestQueue queue = Volley.newRequestQueue(this.chatListInflate.getContext());
+        try {
+            StringRequest delete_request = new StringRequest(Request.Method.DELETE,
+                    chat_delete_url + coresChats.getJSONObject(position).getString("conversationID") + '/',
+                    new Response.Listener<String>()
+                    {
+                        @Override
+                        public void onResponse(String response)
+                        {
+                            Log.d("TESTItemListAdapter", "deleted conversation" + response);
+                        }
+                    }, new Response.ErrorListener()
+            {
+                @Override
+                public void onErrorResponse(VolleyError error)
+                {
+                    Log.d("TESTItemListAdapter", error.getMessage());
+                }
+            });
+            queue.add(delete_request);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         coresChats.remove(position);
-        Log.d("TESTChatList", "removing an item");
         notifyDataSetChanged();
     }
 
